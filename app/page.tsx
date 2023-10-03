12319b461ec9869b1d428ce580b94011fe72a307
Utsavthebug <neupaneutsav.2001@gmail.com>
2023-10-03 14:57:12 +0545

    listing and heart component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,34 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getListing from "./actions/getListing";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+import { SafeListing } from "./types";
 
-export default function Home() {
+export default async function Home() {
+  
+  const listings = await getListing()
+  const currentUser = await getCurrentUser()
+
+
+  if(listings.length===0){
+    return <EmptyState showReset />
+  }
+
   return (
-    <div className="text-rose-500 text-2xl">hello airbnb!</div>
-  )
+      <Container>
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3
+        lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 pt-24">
+             {
+              listings.map((listing:SafeListing)=>(
+               <ListingCard  
+               currentUser={currentUser}
+               key={listing.id}
+               data={listing}
+               />
+              ))
+             }
+        </div>
+      </Container>
+    )
 }
